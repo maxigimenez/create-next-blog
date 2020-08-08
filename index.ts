054ab12b8d3957303e2af7f55bef1a0cd651d5e8
Maxi Gimenez <gimenez.maxi@gmail.com>
2020-08-08 09:33:03 +0000

    feat: initial commit

---
+++ index.ts
@@ -0,0 +1,59 @@
+#!/usr/bin/env node
+import chalk from 'chalk';
+import Commander from 'commander';
+import path from 'path';
+import validateNpmName from 'validate-npm-package-name';
+
+import packageJson from './package.json';
+
+let projectPath: string = '';
+
+const program = new Commander.Command(packageJson.name)
+  .version(packageJson.version)
+  .arguments('<project-directory>')
+  .usage(`${chalk.green('<project-directory>')} [options]`)
+  .action((name) => projectPath = name)
+  .allowUnknownOption()
+  .parse(process.argv);
+
+const run = async () => {
+  if (typeof projectPath === 'string') {
+    projectPath = projectPath.trim();
+  }
+
+  if (!projectPath) {
+    console.log();
+    console.log('Please specify the project directory:');
+    console.log(
+    `   ${chalk.cyan(program.name())} ${chalk.green('<project-directory>')}`
+    );
+    console.log('For example:');
+    console.log(`   ${chalk.cyan(program.name())} ${chalk.green('my-blog')}`);
+    console.log();
+    return;
+  }
+
+  const resolvedProjectPath = path.resolve(projectPath);
+  const projectName = path.basename(resolvedProjectPath);
+
+  const validation = validateNpmName(projectName);
+  if (!validation.validForNewPackages) {
+    console.error(
+      `Could not create a project called ${chalk.red(
+        `"${projectName}"`
+      )} because of npm naming restrictions:`
+    );
+
+    validation.errors!.forEach((p) => console.error(`    ${chalk.red.bold('*')} ${p}`));
+    process.exit(1);
+  }
+
+  
+}
+
+run()
+  .catch((reason) => {
+    console.log();
+    console.log('Aborting installation.');
+    process.exit(1);
+  })
